155bcd77986e8c9abb73b6f98afcc5d2428c8ec8
Ameen Sid <ameensid7@outlook.com>
2026-03-14 21:39:38 +0530

    Added port field in database configuration

--- src/database/db.js
+++ src/database/db.js
@@ -3,18 +3,19 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const sqlConfig = {
-  user: process.env.DB_USER || 'sa', // 'sa' is default admin for MSSQL
+  user: process.env.DB_USER || 'sa',
   password: process.env.DB_PASSWORD || 'root',
   database: process.env.DB_NAME || 'print_slip_app',
   server: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT) || 1433,
   pool: {
     max: 10,
     min: 0,
     idleTimeoutMillis: 30000
   },
   options: {
-    encrypt: false, // Set to true if you are hosting on Azure
-    trustServerCertificate: true // Required for local development / self-signed certs
+    encrypt: false,
+    trustServerCertificate: true
   }
 };
 
